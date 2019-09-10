cd733de31e29c62058c8dff4a84f2cb2e74d10df
muzhawir <muzhawir.amri@gmail.com>
2019-09-11 01:25:58 +0800

    Create Lectures table

---
+++ database/migrations/2019_09_10_165539_create_lectures_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('room_id');
+            $table->unsignedInteger('lecture_hour_id');
+            $table->unsignedInteger('lecturer_id');
+            $table->unsignedInteger('course_id');
+            $table->timestamps();
+
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreign('lecture_hour_id')->references('id')->on('lecture_hours');
+            $table->foreign('lecturer_id')->references('id')->on('lecturers');
+            $table->foreign('course_id')->references('id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures', function (Blueprint $table) {
+            $table->dropForeign('room_id');
+            $table->dropForeign('lecture_hour_id');
+            $table->dropForeign('lecturer_id');
+            $table->dropForeign('course_id');
+        });
+    }
+}
