7650d4cd5926ebd3e2751fd188867defc9178433
muzhawir <muzhawir.amri@gmail.com>
2019-09-19 15:23:06 +0800

    Add Course and Lecturer Controller

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Course;
+use Redirect, Response;
+use Datatables;
+
+class CourseController extends Controller
+{
+    public function getData()
+    {
+        return Datatables::of(Course::all())
+        ->addIndexColumn()
+        ->addColumn('action', function($courses){
+            $actions =
+            '<div class="kt-align-center">'.
+                '<span class="dropdown" title="Pilihan">'.
+                    '<a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" data-toggle="dropdown" aria-expanded="false">'.
+                    '<i class="flaticon2-menu-4"></i>'.
+                    '</a>'.
+                    '<div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-32px, 27px, 0px);">'.
+                    '<form action="'.route('course.destroy', $courses->id).'" method="POST">'.
+                        '<input type="hidden" name="_token" value="'.csrf_token().'">'.
+                        '<input type="hidden" name="_method" value="DELETE">'.
+                        '<button type="submit" class="dropdown-item kt-font-bolder kt-font-danger" OnClick="return confirm(\'Hapus data ini ?\')"><i class="la la-remove kt-font-danger"></i> Hapus</button>'.
+                        '<a class="dropdown-item kt-font-bolder kt-font-success" href="'.route('course.edit', $courses->id).'"><i class="la la-edit kt-font-success"></i> Ubah</a>'.
+                    '</form>'.
+                    '</div>'.
+                '</span>'.
+                '<a href="'. route('course.show', $courses->id) .'" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Lihat Detail">'.
+                    '<i class="flaticon2-search"></i>'.
+                '</a>'.
+            '</div>';
+            return $actions;
+        })
+        ->rawColumns(['action'])
+        ->make(true);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.course.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.course.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|max:255',
+            'name' => 'required|max:255',
+            'credit' => 'required|max:255|integer'
+        ]);
+
+        Course::create($request->all());
+
+        return redirect()->route('course.index')->with('success', 'Data mata kuliah berhasil ditambah.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Course $course)
+    {
+        return view('admin.course.show', compact('course'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Course $course)
+    {
+        return view('admin.course.edit', compact('course'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Course $course)
+    {
+        $request->validate([
+            'code' => 'required|max:255',
+            'name' => 'required|max:255',
+            'credit' => 'required|max:255'
+        ]);
+
+        $course->update($request->all());
+
+        return redirect()->route('course.index')->with('success', 'Data mata kuliah berhasil diubah.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Course $course)
+    {
+        $course->delete();
+        return redirect()->route('course.index')->with('success', 'Mata kuliah berhasil dihapus.');
+    }
+}
---
+++ app/Http/Controllers/LecturerController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Lecturer;
+use Redirect, Response;
+use Datatables;
+
+class LecturerController extends Controller
+{
+    public function getData()
+    {
+        return Datatables::of(Lecturer::all())
+        ->addIndexColumn()
+        ->addColumn('action', function($lecturers){
+            $actions =
+            '<div class="kt-align-center">'.
+                '<span class="dropdown" title="Pilihan">'.
+                    '<a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" data-toggle="dropdown" aria-expanded="false">'.
+                    '<i class="flaticon2-menu-4"></i>'.
+                    '</a>'.
+                    '<div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-32px, 27px, 0px);">'.
+                    '<form action="'.route('lecturer.destroy', $lecturers->id).'" method="POST">'.
+                        '<input type="hidden" name="_token" value="'.csrf_token().'">'.
+                        '<input type="hidden" name="_method" value="DELETE">'.
+                        '<button type="submit" class="dropdown-item kt-font-bolder kt-font-danger" OnClick="return confirm(\'Hapus data ini ?\')"><i class="la la-remove kt-font-danger"></i> Hapus</button>'.
+                        '<a class="dropdown-item kt-font-bolder kt-font-success" href="'.route('lecturer.edit', $lecturers->id).'"><i class="la la-edit kt-font-success"></i> Ubah</a>'.
+                    '</form>'.
+                    '</div>'.
+                '</span>'.
+                '<a href="'. route('lecturer.show', $lecturers->id) .'" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="Lihat Detail">'.
+                    '<i class="flaticon2-search"></i>'.
+                '</a>'.
+            '</div>';
+            return $actions;
+        })
+        ->rawColumns(['action'])
+        ->make(true);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.lecturer.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.lecturer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nidn' => 'required|max:255',
+            'name' => 'required|max:255',
+            'gender' => 'required|max:1',
+            'degree' => 'required|max:4'
+
+        ]);
+
+        Lecturer::create($request->all());
+
+        return redirect()->route('lecturer.index')->with('success', 'Data dosen berhasil ditambah.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lecturer $lecturer)
+    {
+        return view('admin.lecturer.show', compact('lecturer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lecturer $lecturer)
+    {
+        return view('admin.lecturer.edit', compact('lecturer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lecturer $lecturer)
+    {
+        $request->validate([
+            'nidn' => 'required|max:255',
+            'name' => 'required|max:255',
+            'gender' => 'required|max:1',
+            'degree' => 'required|max:4'
+        ]);
+
+        $lecturer->update($request->all());
+
+        return redirect()->route('lecturer.index')->with('success', 'Data dosen berhasil diubah.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lecturer $lecturer)
+    {
+        $lecturer->delete();
+        return redirect()->route('lecturer.index')->with('success', 'Data dosen berhasil dihapus.');
+    }
+}
