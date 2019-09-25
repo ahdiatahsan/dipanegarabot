9cc82e255f2da969a1c009844dcab854057ece1b
muzhawir <muzhawir.amri@gmail.com>
2019-09-25 18:24:55 +0800

    Fix email validate rule

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -130,7 +130,7 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|email|unique:users,email,'.$user->id.',id',
             'password' => 'required|string|min:6|confirmed',
             'role_id' => 'required|max:2',
         ]);
