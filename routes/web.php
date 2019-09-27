935bb148f35d8b0d62a9394ecdaa277c07f0ff3a
muzhawir <muzhawir.amri@gmail.com>
2019-09-28 01:13:15 +0800

    Sort Route List

--- routes/web.php
+++ routes/web.php
@@ -14,36 +14,34 @@
 Route::get('/', function(){
     return view('auth.login');
 })->middleware('guest');
+
 Auth::routes();
 Auth::routes([ 'register' => false ]);
-Route::get('/botman/tinker', 'BotManController@tinker');
 Route::any('getCourse', 'CourseController@getData')->name('course.getData');
+Route::any('getCourseList', 'CourseController@getList')->name('course.getList');
 Route::any('getFile', 'FileController@getData')->name('file.getData');
+Route::any('getFileList', 'FileController@getList')->name('file.getList');
 Route::any('getInformation', 'InformationController@getData')->name('information.getData');
 Route::any('getInformationCategory', 'InformationCategoryController@getData')->name('informationCategory.getData');
+Route::any('getInformationList', 'InformationController@getList')->name('information.getList');
 Route::any('getLecture', 'LectureController@getData')->name('lecture.getData');
 Route::any('getLectureHour', 'LectureHourController@getData')->name('lectureHour.getData');
+Route::any('getLectureList', 'LectureController@getList')->name('lecture.getList');
 Route::any('getLecturer', 'LecturerController@getData')->name('lecturer.getData');
+Route::any('getLecturerList', 'LecturerController@getList')->name('lecturer.getList');
 Route::any('getRole', 'RoleController@getData')->name('role.getData');
 Route::any('getRoom', 'RoomController@getData')->name('room.getData');
 Route::any('getUser', 'UserController@getData')->name('user.getData');
 Route::get('/botman/tinker', 'BotManController@tinker');
-Route::match(['get', 'post'], '/botman', 'BotManController@handle');
-
-
-Route::get('lecturerList', 'LecturerController@list');
+Route::get('/botman/tinker', 'BotManController@tinker');
 Route::get('courseList', 'CourseController@list');
-Route::get('lectureList', 'LectureController@list');
-Route::get('informationList', 'InformationController@list');
 Route::get('fileList', 'FileController@list');
-Route::get('roomList', 'RoomController@list');
-
-Route::any('getLecturerList', 'LecturerController@getList')->name('lecturer.getList');
-Route::any('getCourseList', 'CourseController@getList')->name('course.getList');
-Route::any('getLectureList', 'LectureController@getList')->name('lecture.getList');
-Route::any('getInformationList', 'InformationController@getList')->name('information.getList');
-Route::any('getFileList', 'FileController@getList')->name('file.getList');
 Route::get('getRoomList', 'RoomController@getList')->name('room.getList');
+Route::get('informationList', 'InformationController@list');
+Route::get('lectureList', 'LectureController@list');
+Route::get('lecturerList', 'LecturerController@list');
+Route::get('roomList', 'RoomController@list');
+Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 
 Route::group(['middleware' => ['auth','superAdmin']], function () {
     Route::resource('role', 'RoleController');
